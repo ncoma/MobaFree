c71921ef9e50f7cc6fe689ec46292143967782cb
ncoma <jpms9308@gmail.com>
2014-11-19 14:17:29 -0600

    second commit

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\workspace\Ionic\mobaFree - copia\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
-C:\workspace\Ionic\mobaFree - copia\platforms\android\CordovaLib\AndroidManifest.xml \
--- platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\workspace\Ionic\mobaFree - copia\platforms\android\ant-build\AndroidManifest.xml : \
-C:\workspace\Ionic\mobaFree - copia\platforms\android\AndroidManifest.xml \
